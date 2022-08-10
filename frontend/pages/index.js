5025bcb41f6764acfbdff77d8bbd342ae2381e60
Umar Esso <umarkhatab467@gmail.com>
2022-08-10 06:17:47 +0500

    Wallet connection acheived

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,64 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from '../styles/Home.module.css'
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
 
 export default function Home() {
   const maxTotalSupply = 100;
   const totalSupply = 20;
   const [nftBuyCount, setNftBuyCount] = useState(1);
-
+  const [connected, setConnected] = useState(false);
   let maxNftBuyCount = 10;
+
+
+  let web3ModalRef = useRef();
+
+  // Credit : LearnWeb3Dao Sophomore track
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Rinkeby network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  /*
+        connectWallet: Connects the MetaMask wallet
+      */
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+
+
+
+  async function mint() {
+    alert("minting");
+
+  }
 
   function increase() {
     if (nftBuyCount < 10)
@@ -19,6 +69,25 @@ export default function Home() {
       setNftBuyCount(prev => prev - 1);
 
   }
+
+  async function updateInformation() {
+
+  }
+
+  useEffect(() => {
+    if (!connected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      })
+      connectWallet();
+
+    }
+    updateInformation();
+  }, [connected])
+
+
 
 
   return (
@@ -67,8 +136,8 @@ export default function Home() {
             </div>
 
           </div>
-          <div className={styles.nft__mint__button}>
-            MINT NOW
+          <div onClick={connected ? mint : connectWallet} className={styles.nft__mint__button}>
+            {connected ? "MINT NOW" : "CONNECT WALLET"}
           </div>
           <div className={styles.nft__minted}>
             Minted: {totalSupply}/{maxTotalSupply}
